f6378889ef71ce5746b58d09dfd927fbec446e2c
22017107 <22017107@HP-Pavilion-Aero-13>
2023-08-12 16:42:19 +0800

    Done with User portion for main and test (avril)

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
+	private static final int OPTION_QUIT = 1;
+
 	public static void main(String[] args) {
+		//put all variables and arrayLists below here
 
+		//User Class
+		ArrayList<User> userList = new ArrayList<User>();
+		userList.add(new User(101, "User1", "1234", "Admin", 12345678));
+		userList.add(new User(102, "User2", "1234", "Moderator", 23456789));
+		userList.add(new User(103, "User3", "1234", "Helper", 34567890));
 
-//================================= Main Program  =====================================================
-		
+		//================================= Main Program  =====================================================
+
 		int option = 0;
 
 		while (option != 7) {
@@ -13,8 +22,9 @@ public class C206_CaseStudy {
 			C206_CaseStudy.menu();
 			option = Helper.readInt("Enter an option > ");
 
-			if (option == 1) {
+			if (option == OPTION_QUIT) {
 				// User Management
+				userManagement(userList);
 
 			} else if (option == 2) {
 				// Currency Management
@@ -26,8 +36,8 @@ public class C206_CaseStudy {
 
 			} else if (option == 4) {
 				// Account Management
-				
-				
+
+
 			} else if (option == 5) {
 				//Rate Management
 
@@ -35,7 +45,7 @@ public class C206_CaseStudy {
 			} else if (option == 6) {
 				//Feedback Management
 
-				
+
 			} else if (option == 7) {
 				System.out.println("Bye!");
 
@@ -56,20 +66,161 @@ public class C206_CaseStudy {
 		System.out.println("4. Account Management");
 		System.out.println("5. Rate Management");
 		System.out.println("6. Feedback Management");
-		
+		System.out.println("7. Exit System");
+
+
 
 		Helper.line(80, "-");
 
 	}
+
+
 
 	public static void setHeader(String header) {
 		Helper.line(80, "-");
 		System.out.println(header);
 		Helper.line(80, "-");
 	}
+
+	//================================ Create Management system below ==============================================
+
+	//User Management
+	public static void userManagement(ArrayList<User> userList) {
+		userMenu();
+		int option = 0;
+		option = Helper.readInt("Enter an option > ");
+
+		while (option!= 4) {
+			if (option ==1) {
+				User user = inputUser();
+				C206_CaseStudy.addUser(userList, user);
+				System.out.println("User added");
+
+				userMenu();
+				option = Helper.readInt("Enter an option > ");
+
+			}
+			else if (option ==2) {
+				viewUsers(userList);
+				
+				userMenu();
+				option = Helper.readInt("Enter an option > ");
+				
+			}
+			else if (option ==3) {
+				int deleteUser = inputDeleteUser();
+				deleteUser(userList,deleteUser);
+				
+				userMenu();
+				option = Helper.readInt("Enter an option > ");
+
+			}else if (option == 4) {
+				System.out.println("Thank you for using the User Management System!");
+
+			}else {
+				System.out.println("Invalid Option");
+			}
+		}
+
+	}
 	
+	public static void userMenu() {
+		C206_CaseStudy.setHeader("User Management System");
+
+		System.out.println("1. Add new User");
+		System.out.println("2. View all User");
+		System.out.println("3. Delete User");
+		System.out.println("4. Back to Main Menu");
+
+		Helper.line(80, "-");
+	}
+	
+	//User Input Method
+	public static User inputUser(){
+		//input User Details
+		int UserID = Helper.readInt("Enter UserID > ");
+		String username = Helper.readString("Enter Username > ");
+		String pass = Helper.readString("Enter password > ");
+		String role = Helper.readString("Enter User Role > ");
+		int contactInfo = Helper.readInt("Enter Contact Information > ");
+
+		User user= new User(UserID, username, pass, role, contactInfo);
+		return user;
+	}
+	
+	//User Add Method
+	public static void addUser(ArrayList<User> userList, User user) {
+		User loser;
+		for(User u: userList) {
+			loser = u;
+			if (loser.getUserID()== user.getUserID())
+				return;
+		}
+		if ((user.getUsername().isEmpty()) || (user.getRole().isEmpty())|| (user.getPassword().isEmpty())){
+			return;
+		}
+		userList.add(user);
+	}
+	
+	//Retrieve User Method
+	public static String retrieveUser(ArrayList<User> userList) {
+		String output = "";
+
+		for (User user : userList) {
+			output += String.format("%-10s %-15s %-15s %-20d\n", user.getUserID(), user.getUsername(),
+					user.getRole(), user.getContactInfo());
+		}
+		return output;
+	}
+	
+	//View All Users
+	public static void viewUsers(ArrayList<User> userList) {
+		C206_CaseStudy.setHeader("USER LIST");
+		String output = String.format("%-10s %-15s %-15s %-20s\n", "USER ID", "USERNAME",
+				"ROLE", "CONTACT INFORMATION");
+		output += retrieveUser(userList);	
+		System.out.println(output);
+	}
+	
+	//Delete Input Method
+	public static int inputDeleteUser(){
+		int deleteUser = Helper.readInt("Enter the User ID to delete >");
+
+		return deleteUser;
+	}
+	
+	//Delete Users
+	public static void deleteUser(ArrayList<User> userList, int deleteUser) {
+
+		boolean found = false;
+		for (User user:userList) {
+			if (user.getUserID() == deleteUser) {
+				found = true;
+				System.out.println(String.format("%-10s %-15s %-15s %-20s", "USER ID", "USERNAME",
+						"ROLE", "CONTACT INFORMATION"));
+				System.out.println(String.format("%-10s %-15s %-15s %-20d\n", user.getUserID(), user.getUsername(),
+						user.getRole(), user.getContactInfo()));
+
+				String confirm = Helper.readString("Confirm Deletion? (Y/N) >");
+
+				if (confirm.equalsIgnoreCase("y")) {
+					userList.remove(user);
+					System.out.println("User deleted.");
+				} else {
+					System.out.println("Deletion Cancelled");
+				}
+				break;
+			}
+		}
+
+		if (!found) {
+			System.out.println("User ID not found");
+
+		}
+	}
+
 	// Feedback Management
-	
+
 }
 
 
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,125 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	private User user111;
+	private User user211;
+	
+	private ArrayList<User> userList;
+
 
 	@Before
 	public void setUp() throws Exception {
-	}
+		//User Setups
+		user111 = new User(101, "User111", "1234", "Admin", 12345678);
+		user211 = new User(102, "User211", "1234", "Moderator", 234567889);
+		
+		userList = new  ArrayList<User>();
+		
+	
+		
+	}    
+	
+	@Test
+    public void testAddUser() {
+		// User list is not null and it is empty
+		assertNotNull("Test if there is valid userList to add to", userList);
+		assertEquals("Test that the userList is empty.", 0, userList.size());
 
+		//Given an empty list, after adding 1 User, the size of the list is 1
+		C206_CaseStudy.addUser(userList, user111);		
+		assertEquals("Test that the userList size is 1.", 1, userList.size());
+		
+		
+		// Add an item
+		C206_CaseStudy.addUser(userList, user211);
+		assertEquals("Test that the userList size is now 2.", 2, userList.size());
+		//The item just added is as same as the last item in the list
+		assertSame("Test that user is added to the end of the list.", user211, userList.get(1));
+
+		// Add an item that already exists in the list
+		C206_CaseStudy.addUser(userList, user211);
+		assertEquals("Test that the userList size is unchanged.", 2, userList.size());
+
+		// Add an item that has missing detail
+		User user_missing = new User(999, "User199", "", "Admin", 99999999);
+		C206_CaseStudy.addUser(userList, user_missing);
+		assertEquals("Test that the userList size is unchanged.", 2, userList.size());
+    }
+
+    @Test
+    public void testViewUser() {
+    	
+		//Normal Condition: View Empty List
+		// Test if Item list is not null and empty
+		assertNotNull("Test if there is valid userList to add to", userList);
+		assertEquals("Test that the userList is empty.", 0, userList.size());
+		
+		// Attempt to retrieve the Users 
+		String retrieveUser= C206_CaseStudy.retrieveUser(userList);
+		String testOutput = "";
+		// Test if the output is empty
+		assertEquals("Test that nothing is displayed", testOutput, retrieveUser);
+
+		//Boundary Condition: Add Users to List & View
+		C206_CaseStudy.addUser(userList, user111);
+		C206_CaseStudy.addUser(userList, user211);
+		// Test that the list is not empty
+		assertEquals("Test that Camcorder arraylist size is 2.", 2, userList.size());
+		
+		// Attempt to retrieve the Users 
+		retrieveUser= C206_CaseStudy.retrieveUser(userList);
+		
+		testOutput = String.format("%-10s %-15s %-15s %-20d\n", 101, "User111", "Admin", 12345678);
+		testOutput += String.format("%-10s %-15s %-15s %-20d\n",102, "User211", "Moderator", 234567889);
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, retrieveUser);
+		
+	    // Error condition: Incomplete User View
+        User incompleteUser = new User(104, "", "", "", 12341234);
+        
+		C206_CaseStudy.addUser(userList, incompleteUser);
+        
+		assertEquals("Test that Chromebook arraylist size is 2.", 2, userList.size());
+
+        // Attempt to retrieve the Users again
+        retrieveUser= C206_CaseStudy.retrieveUser(userList);
+        // Test that the output is still correct after errors
+        assertEquals("Test that the display is correct", testOutput, retrieveUser);
+
+    }
+
+    @Test
+    public void testDeleteUser() {
+		C206_CaseStudy.addUser(userList, user111);
+		C206_CaseStudy.addUser(userList, user211);
+
+        // Normal condition: Delete existing user
+        C206_CaseStudy.deleteUser(userList, user111.getUserID());
+        assertEquals("Test that the user1 is removed from the list.", 1, userList.size());
+
+        // Boundary condition: Delete until List is empty
+        C206_CaseStudy.deleteUser(userList, user211.getUserID());
+        assertEquals("Test that the list is empty.", 0, userList.size());
+        
+		C206_CaseStudy.addUser(userList, user111);
+
+        // Error condition: Attempt to delete with negative user ID
+        C206_CaseStudy.deleteUser(userList, 103);
+        assertEquals("Test that the list size remains the same.", 1, userList.size());
+    }
+    
+	
 	@After
 	public void tearDown() throws Exception {
+		//User Teardown
+		user111 = null;
+		user211 = null;
+		userList = null;
 	}
-
-	@Test
-	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
-	}
-
 }
