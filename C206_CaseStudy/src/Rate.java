a9a751a847fd0a449febb303fc0f40ff06b7e9dc
phoen <phoen@phoenix>
2023-08-14 11:57:56 +0800

    Rate Class

---
+++ C206_CaseStudy/src/Rate.java
@@ -0,0 +1,65 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: phoen
+ * Student ID: {type your id here}
+ * Class: {type your class here}
+ * Date/Time created: Monday 14-08-2023 00:57
+ */
+
+/**
+ * @author phoen
+ *
+ */
+public class Rate {
+	
+private String CurrencyName; 
+private String UniqueCurrencyCode;
+private double  ExchangeRate;
+/**
+ * @param string
+ * @param string2
+ * @param string3
+ */
+public Rate(String string, String string2, String string3) {
+	// TODO Auto-generated constructor stub
+}
+/**
+ * @return the currencyName
+ */
+public String getCurrencyName() {
+	return CurrencyName;
+}
+/**
+ * @param currencyName the currencyName to set
+ */
+public void setCurrencyName(String currencyName) {
+	CurrencyName = currencyName;
+}
+/**
+ * @return the uniqueCurrencyCode
+ */
+public String getUniqueCurrencyCode() {
+	return UniqueCurrencyCode;
+}
+/**
+ * @param uniqueCurrencyCode the uniqueCurrencyCode to set
+ */
+public void setUniqueCurrencyCode(String uniqueCurrencyCode) {
+	UniqueCurrencyCode = uniqueCurrencyCode;
+}
+/**
+ * @return the exchangeRate
+ */
+public double getExchangeRate() {
+	return ExchangeRate;
+}
+/**
+ * @param exchangeRate the exchangeRate to set
+ */
+public void setExchangeRate(double exchangeRate) {
+	ExchangeRate = exchangeRate;
+}
+}
